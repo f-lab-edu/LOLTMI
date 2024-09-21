c08f5cb9992bfe565b4bee23dd476157548cc31c
tlsrl6427 <tlsr6427@naver.com>
2024-09-21 17:49:46 +0900

    Multi Module init2

--- batch/src/main/java/com/loltmi/BatchApplication.java
+++ batch/src/main/java/com/loltmi/BatchApplication.java
@@ -1,20 +1,20 @@
-/*package com.loltmi;
+package com.loltmi;
 
 import jakarta.annotation.PostConstruct;
+import javax.swing.Spring;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@EnableBatchProcessing
 @SpringBootApplication
-@RequiredArgsConstructor
+@Slf4j
 public class BatchApplication {
-
-    private final TestBean testBean;
-    @PostConstruct
-    public void dependencyTest(){
-        testBean.dependencyTest();
-    }
     public static void main(String[] args) {
-        SpringApplication.run(ApiApplication.class, args);
+        int exit = SpringApplication.exit(SpringApplication.run(BatchApplication.class, args));
+        log.info("exit = {}", exit);
+        System.exit(exit);
     }
-}*/
+}
