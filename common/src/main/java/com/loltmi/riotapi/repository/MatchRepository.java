5bb1be2a8994ba274767c8d8b6c9f986837498f2
tlsrl6427 <tlsr6427@naver.com>
2024-10-13 14:35:57 +0900

    FEAT: Matches existsById 쿼리 수정(count -> limit 1)

--- common/src/main/java/com/loltmi/riotapi/repository/MatchRepository.java
+++ common/src/main/java/com/loltmi/riotapi/repository/MatchRepository.java
@@ -2,6 +2,23 @@ package com.loltmi.riotapi.repository;
 
 import com.loltmi.riotapi.entity.Matches;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface MatchRepository extends JpaRepository<Matches, String> {
+
+    @Query(value = """
+		select 
+        	case when exists(
+            	select 1 
+                from matches m
+                where m.id = :id
+            )
+            then 'true'
+            else 'false'
+            end
+        """,
+        nativeQuery = true
+    )
+    boolean existsById(@Param("id") String id);
 }
