fb75357847312c411308674c73cf4c5a3a828d73
tlsrl6427 <tlsr6427@naver.com>
2025-04-19 18:43:24 +0900

    Feat: MongoDB Document 생성

---
+++ api/src/main/java/com/loltmi/document/Card/Cards.java
@@ -0,0 +1,42 @@
+package com.loltmi.document.Card;
+
+import jakarta.persistence.Id;
+import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "cards")
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Getter
+public class Cards extends BaseDocument{
+
+    @Id
+    private String id;
+    private String name;
+    private int hits;
+    private List<Contents> contents;
+
+    @NoArgsConstructor
+    @AllArgsConstructor
+    @Builder
+    @Getter
+    public static class Contents{
+        private String title;
+        private String name;
+        private String imgUrl;
+        private double value;
+
+        public void changeValue(double value){
+            this.value = value;
+        }
+    }
+
+
+}
+
